4e9cda6b7c35157e266a45110628adf17e0d64a4
JozefKysel <kysel.jojo@gmailcom>
2019-06-13 19:37:34 +0200

    yes

---
+++ index.js
@@ -0,0 +1,8 @@
+// Return an English translated sentence of the passed binary string.
+// binaryAgent("01101000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100 0001010") 
+// should return "hello world") 
+// binaryAgent("01101000 01100101 01101100 01101100 01101111 0001010") should return "hello"
+
+exports.botTranslator = (binaryString) => {
+  
+}; 

f4eb1d6e82de8cc8f9a5dc9909ce076d8b10fcb0
JozefKysel <kysel.jojo@gmailcom>
2019-06-13 19:53:51 +0200

    ýes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Return an English translated sentence of the passed binary string.
-// binaryAgent("01101000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100 0001010") 
-// should return "hello world") 
-// binaryAgent("01101000 01100101 01101100 01101100 01101111 0001010") should return "hello"
+// binaryAgent("01101000 01100101 01101100 01101100 01101111 00100000 01110111 01101111 01110010 01101100 01100100 0001010")
+// should return "hello world")
 
-exports.botTranslator = (binaryString) => {
+// binaryAgent("01101000 01100101 01101100 01101100 01101111 0001010") should return "hello"
+// RUN NPM I first!!!
+
+exports.botTranslator = (str) => {
   
 }; 
--- test.js
+++ test.js
@@ -4,6 +4,6 @@ const {botTranslator} = require('./index.js');
 
 describe('tests', () => {
   it('should return correct translation', () => {
-    botTranslator('01000111 01101111 01101111 01101111 01101111 01100100 00100000 01101010 01101111 01100010').should.eql('Goood job');
+    botTranslator('01100111 01101111 01101111 01101111 01100100 00100000 01101010 01101111 01100010').should.eql('goood job');
   });
 })
